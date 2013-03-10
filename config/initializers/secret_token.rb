e0e7f9a6e054fb2a55d7c138b0822fd2f9d2d3ca
Rafael Williams <rafael.williams@gmail.com>
2013-03-10 12:09:45 -0500

    app joe

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Joe::Application.config.secret_token = '637160adb926d4a22eb6a74d8731a696dd385cb236b69391ed5fb2a87525f6fee168cbd2b01510dc731ccc9f54c9d3bb71d4e74e2403d1cf8b593822110b551c'
